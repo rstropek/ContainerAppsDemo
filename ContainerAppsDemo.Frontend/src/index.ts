c1bd5fa7d01d1929ca735eecd53c9d060ddc7b48
Rainer Stropek <rainer@software-architects.at>
2022-05-30 21:16:58 +0200

    Enhance config

--- ContainerAppsDemo.Frontend/src/index.ts
+++ ContainerAppsDemo.Frontend/src/index.ts
@@ -3,10 +3,12 @@ import $ from 'cash-dom';
 
 $(() => {
     $('#calculate').on('click', async () => {
+        let baseUrl = $('#base-url').val() as string;
+
         let a = parseInt($('#val-a').val() as string);
         let b = parseInt($('#val-b').val() as string);
 
-        let response = await fetch(`/bff/calculate?a=${a}&b=${b}`);
+        let response = await fetch(`${baseUrl}/bff/calculate?a=${a}&b=${b}`);
         let result = await response.json();
         $('#result').text(`The result is ${result.result}`);
     });
